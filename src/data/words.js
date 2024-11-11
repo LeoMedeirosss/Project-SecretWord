108d44dc92656a23730e027d873e689459d08da0
LeoMedeirosss <lmf@cesar.school>
2024-11-11 17:30:31 -0300

    reseting css and adding a data folder

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante"],
+    fruta: ["Banana", "Maçã", "Uva", "Melancia", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Coração", "Olhos", "Pescoço"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador"],
+    programação: ["Python", "Linguagem", "Framework", "JavaScript", "React", "Node"],
+    alimento:["Arroz", "Feijão", "Ovo", "Carne", "Leite", "Soja"]
+};
