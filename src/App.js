6969270c02ff14f1470daecfc95aa662f2f42a49
LeoMedeirosss <lmf@cesar.school>
2024-11-13 16:53:01 -0300

    components 1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { useCallback, useEffect,useState } from 'react';
+
+import{wordsList} from './data/words.js'
+
 import StartScreen from './components/StartScreen.jsx';
 
+const states=[
+  {id:1, name: "start"},
+  {id:2, name: "game"},
+  {id:3, name: "over"}
+];
+
 function App() {
+  const[gameStage, setGameStage] = useState(states[0].name);
+
   return (
     <div className="App">
-        <StartScreen />
+        {gameStage === 'start' && <StartScreen />}
+        {gameStage === 'game' && <StartScreen />}
+        {gameStage === 'over' && <StartScreen />}
     </div>
   );
 }
