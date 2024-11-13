9b4f2ae415525054eeb5447a2fa30c1099a5fc06
LeoMedeirosss <lmf@cesar.school>
2024-11-13 15:46:21 -0300

    landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StartScreen from './components/StartScreen.jsx';
 
 function App() {
   return (
     <div className="App">
-        <h1>Secret Word Game</h1>
+        <StartScreen />
     </div>
   );
 }
