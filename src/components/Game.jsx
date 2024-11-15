8ea4c21390d0b1e09b9849c79653faf890edcfa8
LeoMedeirosss <lmf@cesar.school>
2024-11-15 18:34:19 -0300

    gaming page style

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,9 +2,31 @@ import './Game.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Jogo</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className='game'>
+      <p className='points'>
+        <span>Pontuação: 00</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className='tip'>Dica: <span>Dica</span></h3>
+
+      <div className='wordConteiner'>
+        <span className='letter'>a</span>
+        <span className='whiteSquare'></span>
+      </div>
+
+      <div className='letterConteiner'>
+        <p>Adivinhe uma letra da palavra</p>
+        <form>
+          <input type="text" name='letter' maxLength="1" required/>
+          <button className='botao'>Play</button>
+        </form>
+      </div>
+
+      <div className='wrongLettersConteiner'>
+        <p>Letras já utilizadas</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
