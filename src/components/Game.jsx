48e0d1c63dfd103fe0ad7d3a0345954919830b76
LeoMedeirosss <lmf@cesar.school>
2024-11-19 17:39:57 -0300

    game letter input 1.0

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,32 +1,68 @@
+import { useState, useRef } from 'react'
 import './Game.css'
 
-const Game = ({verifyLetter}) => {
+const Game = ({
+  verifyLetter,
+  pickedWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters, 
+  guesses,
+  score,
+}) => {
+  const [letter, setLetter] = useState('');
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    verifyLetter(letter);
+    setLetter("");
+    letterInputRef.current.focus();
+  }
+
   return (
+
     <div className='game'>
       <p className='points'>
-        <span>Pontuação: 00</span>
+        <span>Pontuação: {score}</span>
       </p>
       <h1>Adivinhe a palavra</h1>
-      <h3 className='tip'>Dica: <span>Dica</span></h3>
-      <p>Tentativas restantes: <strong>X</strong></p>
+      <h3 className='tip'>Dica: <span>{pickedCategory}</span></h3>
+        <p>Tentativas restantes: <strong>{guesses}</strong></p>
 
       <div className='wordConteiner'>
-        <span className='letter'>a</span>
-        <span className='whiteSquare'></span>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+            <span key={1} className='letter'>
+              {letter}
+            </span>
+          ) : (
+            <span key={1} className='whiteSquare'></span>
+          )
+        )}
       </div>
 
       <div className='letterConteiner'>
         <p>Adivinhe uma letra da palavra</p>
-        <form>
-          <input type="text" name='letter' maxLength="1" required/>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            name='letter' 
+            maxLength="1" 
+            required onChange={(e)=> setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+            />
           <button>Play</button>
         </form>
       </div>
 
       <div className='wrongLettersConteiner'>
         <p>Letras já utilizadas</p>
-        <span>a,</span>
-        <span>b,</span>
+        {wrongLetters.map((letter,i) =>(
+          <span key={i}>{letter},</span>
+        ))}
       </div>
     </div>
   )
