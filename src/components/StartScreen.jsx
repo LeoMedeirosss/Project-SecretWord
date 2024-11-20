adfe92818e6875e605abf4c3c5f74e564f0569f1
LeoMedeirosss <lmf@cesar.school>
2024-11-20 16:03:48 -0300

    game - final 2.0

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -2,11 +2,22 @@ import './StartScreen.css';
 
 const StartScreen = ({startGame}) => {
   return (
-    <div className='start'>
-      <h1>Secret Word</h1>
-      <p>Aperte no botão abaixo para começar!</p>
-      <button onClick={startGame}>Começar o jogo</button>
-    </div>
+    <>
+      <div>
+        <div className='by'>
+          <h4>Done by: <a 
+          href="https://github.com/LeoMedeirosss" 
+          target='blank'
+          aria-hidden="true">LeoMedeirosss</a></h4>
+        </div>
+        <div className='start'>
+          <h1 className='title'>Secret Word</h1>
+          <p>Aperte no botão abaixo para começar!</p>
+         <button onClick={startGame}>Começar o jogo</button>
+        </div>
+      </div>
+    </>
+    
   )
 }
 
